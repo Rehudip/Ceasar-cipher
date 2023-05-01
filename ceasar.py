d1c7d661c1584279cea1be5385f52de1da58b59a
rehudip <racingcup3@gmail.com>
2023-05-01 17:12:00 +0900

    EDIT: ceasar.py
    functionized ceasr.py

--- ceasar.py
+++ ceasar.py
@@ -1,19 +1,24 @@
 
 word = 'a quick brown fox jumps over the lazy dog'
 adjust = 3
-ciphered_word = ''
 
-for a in word:
-  ceasar_letter = ord(a)
-  if ceasar_letter == 32:
-    pass
-  #IF UPPERCASE
-  elif 65 <= ceasar_letter < 91:
-    ceasar_letter = ((ceasar_letter - 65 + adjust) % 26) + 65
-  #if lowercase
-  elif 97 <= ceasar_letter < 123:
-    ceasar_letter = ((ceasar_letter - 97 + adjust) % 26) + 97
-  else:
-    break
-  ciphered_word = ciphered_word + chr(ceasar_letter)
-print(ciphered_word)
+def cipher(word,adjust=3):
+  ciphered_word = ''
+  for a in word:
+    ceasar_letter = ord(a)
+    if ceasar_letter == 32:
+      pass
+    #IF UPPERCASE
+    elif 65 <= ceasar_letter < 91:
+      ceasar_letter = ((ceasar_letter - 65 + adjust) % 26) + 65
+    #if lowercase
+    elif 97 <= ceasar_letter < 123:
+      ceasar_letter = ((ceasar_letter - 97 + adjust) % 26) + 97
+    else:
+      break
+    ciphered_word = ciphered_word + chr(ceasar_letter)
+  #print(ciphered_word)
+
+  return ciphered_word
+
+print(cipher(word,adjust))
