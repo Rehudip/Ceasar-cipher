2a1fa5bc75ca73fd0935a4aa6351bb4bd05611f5
rehudip <racingcup3@gmail.com>
2023-05-01 17:06:54 +0900

    EDIT: ceasar.py
    implementing basic ciphering function

--- ceasar.py
+++ ceasar.py
@@ -1,3 +1,19 @@
-def ceasar(word):
-	print(word)
-ceasar('Hello')
+
+word = 'a quick brown fox jumps over the lazy dog'
+adjust = 3
+ciphered_word = ''
+
+for a in word:
+  ceasar_letter = ord(a)
+  if ceasar_letter == 32:
+    pass
+  #IF UPPERCASE
+  elif 65 <= ceasar_letter < 91:
+    ceasar_letter = ((ceasar_letter - 65 + adjust) % 26) + 65
+  #if lowercase
+  elif 97 <= ceasar_letter < 123:
+    ceasar_letter = ((ceasar_letter - 97 + adjust) % 26) + 97
+  else:
+    break
+  ciphered_word = ciphered_word + chr(ceasar_letter)
+print(ciphered_word)
